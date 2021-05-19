a8b0aac067e4c254aa788922710b0c2129e9a952
Nico Abrasado <>
2021-05-19 00:55:01 +0000

    Added exit button, added sticky arrangement to allow 2 buttons on the same row without the use of columns

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 class QuizStarter: # Class for the UI, the main menu and username 
     def __init__(self, parent):
-        background_color = "Dodgerblue4"
+        background_color = "Dodgerblue4" 
         # Frame of the UI
         self.quiz_frame = Frame(parent, bg=background_color, padx=100, pady=100)
         # Geometry and placement of the UI
@@ -23,7 +23,11 @@ class QuizStarter: # Class for the UI, the main menu and username
       
         #continue button
         self.continue_button = Button(self.quiz_frame, text="Continue", font=("Helvetica", "13", "bold"), bg="orange")
-        self.continue_button.grid(row=3,  padx=100, pady=20)  
+        self.continue_button.grid(row=3,  padx=50, pady=20, sticky=E)  
+
+        #Exit button 
+        self.exit_button = Button(self.quiz_frame, text="Exit", font=("Helvetica", "13", "bold"), bg="orange")
+        self.exit_button.grid(row=3, padx=50,pady=20, sticky=W)
                   
 if __name__ == "__main__":
   root = Tk()
