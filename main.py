17f3cf5c65b134d380c223707eef7d7a33a786c3
Nico Abrasado <>
2021-05-20 00:34:00 +0000

    Tweaked the colours and changed the colour scheme as well as the padding between different elements. Changed the text to suit the actual goal of the program more

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from tkinter import *
 
 class QuizStarter: # Class for the UI, the main menu and username 
     def __init__(self, parent):
-        background_color = "Dodgerblue4" 
+        background_color = "gray13" 
+        foreground_color = "floralwhite"
         # Frame of the UI
         self.quiz_frame = Frame(parent, bg=background_color, padx=100, pady=100)
         # Geometry and placement of the UI
@@ -10,24 +11,24 @@ class QuizStarter: # Class for the UI, the main menu and username
 
         #Widgets
         # Main header
-        self.heading_label = Label(self.quiz_frame, text="Lorem ipsum dolor sit amet",font=("Tw Cen MT","18","bold"),bg=background_color)
+        self.heading_label = Label(self.quiz_frame, text="NCEA Study Buddy",font=("Tw Cen MT","20","bold"),bg=background_color,fg=foreground_color)
         self.heading_label.grid(row=0, padx=100)
 
         # Username Label
-        self.user_label = Label(self.quiz_frame, text="Lorem ipsum dolor sit amet", font=("Tw Cen MT","16"),bg=background_color)
-        self.user_label.grid(row=1,padx=100)
+        self.user_label = Label(self.quiz_frame, text="Enter your username", font=("Tw Cen MT","14"),bg=background_color, fg=foreground_color)
+        self.user_label.grid(row=1,pady=20,padx=100)
         
         #entry box
         self.entry_box=Entry(self.quiz_frame)
-        self.entry_box.grid(row=2,padx=100, pady=40)
+        self.entry_box.grid(row=2,padx=100, pady=20)
       
         #continue button
-        self.continue_button = Button(self.quiz_frame, text="Continue", font=("Helvetica", "13", "bold"), bg="orange")
-        self.continue_button.grid(row=3,  padx=50, pady=20, sticky=E)  
+        self.continue_button = Button(self.quiz_frame, text="Continue", font=("Helvetica", "13", "bold"), bg="mediumseagreen")
+        self.continue_button.grid(row=3,  padx=50, pady=20, sticky=W)  
 
         #Exit button 
-        self.exit_button = Button(self.quiz_frame, text="Exit", font=("Helvetica", "13", "bold"), bg="orange")
-        self.exit_button.grid(row=3, padx=50,pady=20, sticky=W)
+        self.exit_button = Button(self.quiz_frame, text="Exit", font=("Helvetica", "13", "bold"), bg="mediumseagreen")
+        self.exit_button.grid(row=3, padx=50,pady=20, sticky=E)
                   
 if __name__ == "__main__":
   root = Tk()
