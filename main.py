6858ca4e2ebf9fe37d9da4f2f45fb20f67223a46
Nico Abrasado <84363394+NicoAbrasado@users.noreply.github.com>
2021-06-17 00:04:02 +0000

    Component 3, Trial 6: By adding some new variables such as mechanics_score and pyhthon_score for each category of a subject and reworking the scoring function by checking individually, I was able to make the score system functional.

--- main.py
+++ main.py
@@ -6,7 +6,20 @@ background_color = "gray13"
 foreground_color = "floralwhite"
 btn_color = "mediumseagreen"
 names_list = [] 
-asked = [] 
+asked = []
+
+mechanics_score = 0 
+nuclear_physics_score = 0
+waves_score = 0
+
+python_score = 0
+heuristics_score = 0
+algorithims_score = 0 
+
+physics_categories = [mechanics_score,nuclear_physics_score,waves_score]
+cs_categories = [python_score,heuristics_score,algorithims_score]
+
+
 
 def randomiser(): 
   global qnum 
@@ -24,40 +37,40 @@ randomiser()
 # Physics
 # Question number: Question, Answer 1, Answer 2, Answer 3, Answer 4, Correct Answer placement, Category 
 phy_qanda = {
-  1: ['What is Mechanics?', 'The mechanical components of a car', 'an area of physics concerned with the motion of objects','the interactions between particles','A school of physics which concerns itself with waves',2,'Mechanics'],
-  2: ['Who theorised the existence of electrons?','JJ Thompson','John Dalton','Ernest Rutherford','Issac Newton',1,'Nuclear Physics'],
-  3: ['What are the two different optics in physics?','Light and Sound','Ray and Wave','Dynamic and Mono','Rays and Orbits',2,'Waves'],
-  4: ['What is Newtons first law?','For every action there is an equal and opposire reaction','The amount of acceleration of a body is proportional to the acting force','Gravity acts towards the centre','An object at rest will remain at rest',4,'Mechanics'],
-  5: ['What is the mass of an electron?','1','2','1/1836','1.1836',3,'Nuclear Physics'],
-  6: ['An image is virtual when..','..The rays cross each other','..The rays seem to cross each other','..The image is above the mirror','..The image is in the mirror',2,'Waves'],
-  7: ['How do you calculate centripetal force?','a = V^2/r','F = ma','A = V/t','F = mv^2/r',4,'Mechanics'],
-  8: ['An alpha particle is..','High velocity, but light electrons','Energy from the electromagnetic spectrum','A heavy, slow and positively charged helium nucleus','An ionised particle',3,'Nuclear Physics'],
-  9: ['What is the equation for magnification?','i/O = M','1/i + 1/0 = 1/f = 2/r[f=R/2]','A = V/t','C = 2r',1,'Waves'],
-  10: ['‘For every action there is an equal and opposite reaction’ describes...','Newton’s first law of motion','The Square Cube law','Newton’s fourth law of motion','Newton’s third law of motion',4,'Mechanics'],
-  11: ['What charge is a Beta particle?','Neutral','Positive','All','Negative',4,'Nuclear Physics'],
-  12: ['Wave optics believes that..','..Light is made out of rays','..Light can be split up from rays into waves','..Light is made up of waves','..Light is formed by waves and rays',3,'Waves'],
-  13: ['What forces effected the projectile in it’s motion?','Only Gravity','Gravity and Horozontial force','Gravity and Light','Nothing, it is only directed by the force exerted',1,'Mechanics'],
-  14: ['What is half-life in physics?','Half life is a popular video game franchise','The time it takes for the number of undecayed nuclei to fall to half its original number','The time it takes for decayed nuclei to regenerate','The half point for an organisim',2,'Nuclear Physics'],
-  15: ['What is refraction?','The angle when a ray hits a reflective surface','When a light ray goes from one medium to another','The act of light travelling through space in waves','Light creating an image off of a reflective surface',2,'Waves']
+  1: ['What is Mechanics?', 'The mechanical components of a car', 'an area of physics concerned with the motion of objects','the interactions between particles','A school of physics which concerns itself with waves',2,mechanics_score,'Mechanics'],
+  2: ['Who theorised the existence of electrons?','JJ Thompson','John Dalton','Ernest Rutherford','Issac Newton',1,nuclear_physics_score,'Nuclear Physics'],
+  3: ['What are the two different optics in physics?','Light and Sound','Ray and Wave','Dynamic and Mono','Rays and Orbits',2,waves_score,'Waves'],
+  4: ['What is Newtons first law?','For every action there is an equal and opposire reaction','The amount of acceleration of a body is proportional to the acting force','Gravity acts towards the centre','An object at rest will remain at rest',4,mechanics_score,'Mechanics'],
+  5: ['What is the mass of an electron?','1','2','1/1836','1.1836',3,nuclear_physics_score,'Nuclear Physics'],
+  6: ['An image is virtual when..','..The rays cross each other','..The rays seem to cross each other','..The image is above the mirror','..The image is in the mirror',2,waves_score,'Waves'],
+  7: ['How do you calculate centripetal force?','a = V^2/r','F = ma','A = V/t','F = mv^2/r',4,mechanics_score,'Mechanics'],
+  8: ['An alpha particle is..','High velocity, but light electrons','Energy from the electromagnetic spectrum','A heavy, slow and positively charged helium nucleus','An ionised particle',3,nuclear_physics_score,"Nuclear Physics"],
+  9: ['What is the equation for magnification?','i/O = M','1/i + 1/0 = 1/f = 2/r[f=R/2]','A = V/t','C = 2r',1,waves_score, "Waves"],
+  10: ['‘For every action there is an \n equal and opposite reaction’ describes...','Newton’s first law of motion','The Square Cube law','Newton’s fourth law of motion','Newton’s third law of motion',4,mechanics_score, "Mechanics"],
+  11: ['What charge is a Beta particle?','Neutral','Positive','All','Negative',4,nuclear_physics_score,'Nuclear Physics'],
+  12: ['Wave optics believes that..','..Light is made out of rays','..Light can be split up from rays into waves','..Light is made up of waves','..Light is formed by waves and rays',3,waves_score, "Waves"],
+  13: ['What forces effected the projectile in it’s motion?','Only Gravity','Gravity and Horozontial force','Gravity and Light','Nothing, it is only directed by the force exerted',1,mechanics_score,'Mechanics'],
+  14: ['What is half-life in physics?','Half life is a popular video game franchise','The time it takes for the number of undecayed nuclei to fall to half its original number','The time it takes for decayed nuclei to regenerate','The half point for an organisim',2,nuclear_physics_score,'Nuclear Physics'],
+  15: ['What is refraction?','The angle when a ray hits a reflective surface','When a light ray goes from one medium to another','The act of light travelling through space in waves','Light creating an image off of a reflective surface',2,waves_score,'Waves']
 }
 # Computer Science
 # Question number: Question, Answer 1, Answer 2, Answer 3, Answer 4, Correct Answer placement, Category 
 cs_qanda = {
-  1: ['What is python in computer science?','Python is a programming language','Python is a search engine','Python is a type of snake','Python is a famous car brand',1,'Python'],
-  2: ['What does the heuristic, ‘Match with the real world’ deal with?','Make the user able to edit and change their answers','Use familiar icons and concepts found in the real world','Conventions in programming','Able to show the user the status of the system',2,'Heuristics'],
-  3: ['What is an algorithm?','A version of the windows operating system','A type of programming technique that is similar to a class','A finite ordered sequence of steps to solve a problem','The observation of sequences within a computer program',3,'Algorithms'],
-  4: ['What is the data type for this?: 1.4','Float','Integer','String','Boolean',1,'Python'],
-  5: ['Who made the design heuristics?','Jakob Nielsen','Jacob Nielsen','John Nelson','Rolf Molich',1,'Heuristics'],
-  6: ['When can Algorithms be used?','Only in Python','Any time to design a solution to a problem','When finding an error','Only when repairing computers',2,'Algorithms'],
-  7: ['What does this sign mean in python?: !=','Approximately','If this happens do this','Equal to','Not equal to',4,'Python'],
-  8: ['What is the 9th heuristic?','Aesthetic and Minimalist Design','Recognition Rather than recall','Algorithms and Databases','Diagnose and Recover from errors',4,'Heuristics'],
-  9: ['How do you measure algorithm efficiency in terms of run time and space?','F equations','Small R cubits','Big O Notation','Y5 Rankings',3,'Algorithms'],
-  10: ['What does an If statement do?','Checks if something is true then executes it','Compares 2 values and returns a true or false','While a statement is true, it will loop a indefinitely until it is not','A block of code that only runs if called.',1,'Python'],
-  11: ['What does the Heuristic ‘Diagnose Recover and Errors’ deal with?','Allows access for more efficient processes','Gives the user the freedom to personalise their experience','After making an error, the user will be able to recover easily','Follow already existing conventions',3,'Heuristics'],
-  12: ['What is the worst case for a search and sort algorithm?','O(n)','O(n/2)','F - (QT)','Y5 - 6',1,'Algorithms'],
-  13: ['‘Address = []’ is an example of what in python?','Variable','Integer','Dictionary','List',4,'Python'],
-  14: ['‘Clean, Uncluttered, Visible’ is an example of what heuristic?','Help and Documentation','Flexibility and Efficiency of use','Aesthetic and minimalist design','Consistency and standards',3,'Heuristics'],
-  15: ['What is the best case for a quick sort?','Y4^4','F + (n^6)','n*log(n)','O(f^log(6)',3,'Algorithms']
+  1: ['What is python in computer science?','Python is a programming language','Python is a search engine','Python is a type of snake','Python is a famous car brand',1,python_score,'Python'],
+  2: ['What does the heuristic, ‘Match with the real world’ deal with?','Make the user able to edit and change their answers','Use familiar icons and concepts found in the real world','Conventions in programming','Able to show the user the status of the system',2,heuristics_score,'Heuristics'],
+  3: ['What is an algorithm?','A version of the windows operating system','A type of programming technique that is similar to a class','A finite ordered sequence of steps to solve a problem','The observation of sequences within a computer program',3,algorithims_score,'Algorithms'],
+  4: ['What is the data type for this?: 1.4','Float','Integer','String','Boolean',1,python_score,'Python'],
+  5: ['Who made the design heuristics?','Jakob Nielsen','Jacob Nielsen','John Nelson','Rolf Molich',1,heuristics_score,'Heuristics'],
+  6: ['When can Algorithms be used?','Only in Python','Any time to design a solution to a problem','When finding an error','Only when repairing computers',2,algorithims_score,'Algorithms'],
+  7: ['What does this sign mean in python?: !=','Approximately','If this happens do this','Equal to','Not equal to',4,python_score,'Python'],
+  8: ['What is the 9th heuristic?','Aesthetic and Minimalist Design','Recognition Rather than recall','Algorithms and Databases','Diagnose and Recover from errors',4,heuristics_score,'Heuristics'],
+  9: ['How do you measure algorithm efficiency \n in terms of run time and space?','F equations','Small R cubits','Big O Notation','Y5 Rankings',3,algorithims_score,'Algorithms'],
+  10: ['What does an If statement do?','Checks if something is true then executes it','Compares 2 values and returns a true or false','While a statement is true, it will loop a indefinitely until it is not','A block of code that only runs if called.',1,python_score,'Python'],
+  11: ['What does the Heuristic ‘Diagnose Recover and Errors’ deal with?','Allows access for more efficient processes','Gives the user the freedom to personalise their experience','After making an error, the user will be able to recover easily','Follow already existing conventions',3,heuristics_score,'Heuristics'],
+  12: ['What is the worst case for a search and sort algorithm?','O(n)','O(n/2)','F - (QT)','Y5 - 6',1,algorithims_score,'Algorithms'],
+  13: ['‘Address = []’ is an example of what in python?','Variable','Integer','Dictionary','List',4,python_score,'Python'],
+  14: ['‘Clean, Uncluttered, Visible’ is an example of what heuristic?','Help and Documentation','Flexibility and Efficiency of use','Aesthetic and minimalist design','Consistency and standards',3,heuristics_score,'Heuristics'],
+  15: ['What is the best case for a quick sort?','Y4^4','F + (n^6)','n*log(n)','O(f^log(6)',3,algorithims_score,'Algorithms']
 }
 
 
@@ -211,25 +224,59 @@ class Quiz: # Actual quiz
     self.rb4.config(text = chosen_quiz[qnum][4])
   
   def quiz_progress(self):
+    global mechanics_score
+    global nuclear_physics_score
+    global waves_score
+    global python_score
+    global heuristics_score
+    global algorithms_score
     choice = self.var1.get()
-    if len(asked)>14:
-      if choice == chosen_quiz[qnum][6]:
+    score = chosen_quiz[qnum][6]
+    if len(asked)>14: # For the final question 
+      if choice == chosen_quiz[qnum][5]: # Correct 
         self.confirm_button.config(text='Confirm')
+        score += 1
+    
+        for i in physics_categories:
+          print(i)
         print('Finished')
-      else:
+      else: # Wrong 
         self.confirm_button.config(text='Confirm')
+        for i in physics_categories:
+          print(i)
         print('Finished')
-    else:
-      if choice == 0:
+    else: # If it isn't the final question 
+      if choice == 0: # If the person hasn't selected anything 
         self.error_label.config(text = 'Please select an answer before continuing')
         choice = self.var1.get()
-      else:
-        if choice == chosen_quiz[qnum][6]:
-          print(chosen_quiz[qnum][6])
-          print('Correct')
-          self.questions_setup()
-        else:
-          print(chosen_quiz[qnum][6])
+      else: 
+        if choice == chosen_quiz[qnum][5]: # Correct
+          if chosen_quiz == phy_qanda:
+            if chosen_quiz[qnum][7] == 'Mechanics':
+              mechanics_score += 1
+              print(mechanics_score)
+            elif chosen_quiz[qnum][7] == 'Waves':
+              waves_score += 1
+              print(waves_score)
+            else:
+              nuclear_physics_score += 1
+              print(nuclear_physics_score)
+            print('Correct')
+            self.questions_setup()
+          else:
+            if chosen_quiz[qnum][7] == 'Python':
+              python_score += 1
+              print(python_score)
+            elif chosen_quiz[qnum][7] == 'Algorithms':
+              algorithms_score += 1
+              print(algorithms_score)
+            else:
+              heuristics_score += 1
+              print(heuristics_score)
+            print('Correct')
+            self.questions_setup()
+        else: # Wrong 
+          print(chosen_quiz[qnum][5])
           print('Wrong')
           self.questions_setup()
         
